3cfc905c7d4ec7591714f45646fbbd3d89f7bea8
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-01-11 05:08:06 +0300

    Business kdoları refactor edildi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,5 +13,9 @@ namespace Business.Constants
         public static string ProductsListed = "Ürünler listelendi";
         public static string MaintenanceTime = "Sistem bakımı";
         public static string UnitPriceInvalid = "Geçersiz para birimi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductUpdated = "Ürün güncellendi";
+        public static string ProductNameAlreadyExists = "Ürün adı zaten mevcut";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
